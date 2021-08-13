33e3d46671cde8c309e888df6b753de7fff1fea5
hwar042 <hwar042@aucklanduni.ac.nz>
2021-08-14 11:55:38 +1200

    Add Auth Get/Set

--- src/Auth.java
+++ src/Auth.java
@@ -2,8 +2,38 @@ public class Auth {
     String user;
     String acct;
     String pass;
-    boolean t_user;
-    boolean t_acct;
-    boolean t_pass;
-    boolean auth;
+    boolean t_user = false;
+    boolean t_acct = false;
+    boolean t_pass = false;
+    boolean auth = false;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setAcct(String acct) {
+        this.acct = acct;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setT_user() {
+        t_user = true;
+    }
+
+    public void setT_acct() {
+        t_acct = true;
+    }
+
+    public void setT_pass() {
+        t_pass = true;
+    }
+
+    public void setAuth() {
+        t_user = true;
+        t_acct = true;
+        auth = true;
+    }
 }
