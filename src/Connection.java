b17d52659bcff5c4d6a7c66c7b1b07569f39b740
hwar042 <hwar042@aucklanduni.ac.nz>
2021-08-19 15:00:25 +1200

    ammended imports

--- src/Connection.java
+++ src/Connection.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 public class Connection {
